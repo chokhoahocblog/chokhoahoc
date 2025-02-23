ef2fa2ecd03c88c56fc5831f46d0916b4e842110
chokhoahocblog <chokhoahocblog@gmail.com>
2025-02-23 15:23:46 +0700

    Update [[player]].js

--- functions/[[player]].js
+++ functions/[[player]].js
@@ -1,3 +1,13 @@
-export const onRequest = async ({ next }) => {
-  return next(); // Trả về nội dung của file tĩnh đúng cách
+export const onRequest = async ({ request }) => {
+  const url = new URL(request.url);
+  const response = await fetch(`${url.origin}/index.html`);
+
+  return new Response(response.body, {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 };
