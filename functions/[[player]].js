40584025acede99ca5bfba24f4afa13c505651d2
chokhoahocblog <chokhoahocblog@gmail.com>
2025-02-23 15:25:57 +0700

    Update [[player]].js

--- functions/[[player]].js
+++ functions/[[player]].js
@@ -1,3 +1,20 @@
-export const onRequest = async ({ next }) => {
-  return next(); // Trả về nội dung của file tĩnh đúng cách
+export const onRequest = async ({ request }) => {
+  // Lấy nội dung file index.html từ Cloudflare Pages
+  const url = new URL(request.url);
+  const indexUrl = `${url.origin}/index.html`;
+
+  const response = await fetch(indexUrl);
+
+  // Kiểm tra nếu file tồn tại
+  if (!response.ok) {
+    return new Response("Không tìm thấy trang", { status: 404 });
+  }
+
+  // Trả về nội dung file index.html
+  return new Response(response.body, {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+      "Cache-Control": "no-store",
+    },
+  });
 };
