8d9c8d1c031f751c8aa8c4d83d65ef9cad6fa1d3
chokhoahocblog <chokhoahocblog@gmail.com>
2025-02-23 16:50:28 +0700

    Delete functions/player directory

--- functions/player/[id].js
+++
@@ -1,4 +0,0 @@
-export const onRequest = async (context) => {
-  // Trả về nội dung file index.html khi truy cập /player/ID
-  return context.env.ASSETS.fetch(new Request(new URL("/index.html", context.request.url), context.request));
-};
